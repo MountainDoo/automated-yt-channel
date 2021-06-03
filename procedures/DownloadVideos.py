8734166806c911f8c2d6383e4a383a7f5aefd683
MountainDoo <64296620+MountainDoo@users.noreply.github.com>
2021-06-02 20:41:11 -0400

    Create DownloadVideos.py

---
+++ procedures/DownloadVideos.py
@@ -0,0 +1,28 @@
+'''
+1. Download daily trending TikTok clips
+'''
+from TikTokApi import TikTokApi
+import string
+import random
+import os
+#verifyFp might need to change daily
+DAILY_TRENDING_DIR = "directory location for downloaded tiktok videos"
+verifyFp = 'use s_v_web_id cookie from tiktok.com'
+did = ''.join(random.choice(string.digits) for num in range(19))
+
+api = TikTokApi.get_instance(custom_verifyFp=verifyFp, use_test_endpoints=True)
+
+tiktoks = api.trending()
+video_bytes = api.get_Video_By_TikTok(tiktoks[0])
+
+def download_tiktoks():
+    os.chdir(DAILY_TRENDING_DIR)
+    for tiktok in tiktoks:
+        video_bytes = api.get_Video_By_TikTok(tiktok)
+        author = tiktok['author']['uniqueId']
+        #print(author)
+        with open(str(author) + '.mp4', 'wb') as o:
+            o.write(video_bytes)
+        print('Downloading: ' + tiktok['author']['uniqueId'])
+        
+ 
